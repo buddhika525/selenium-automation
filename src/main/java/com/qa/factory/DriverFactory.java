ef8c2ed457298502f9b2ce3a7d461ce7aada218a
buddhikatillekaratne <buddhikat525@gmail.com>
2024-07-19 08:24:45 +0530

    Initialized webdriver, added property file and cucumber hooks

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -1,5 +1,37 @@
 package com.qa.factory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DriverFactory {
+	public static ThreadLocal<WebDriver> tldriver = new ThreadLocal<>();
+	
+	//initialise the theardloacl driver
+		public WebDriver initDriver(String browser) {
+			if(browser.equals("chrome")) {
+				WebDriverManager.chromedriver().setup();
+				tldriver.set(new ChromeDriver());
+			}
+			
+			else if(browser.equals("firefox")) {
+				WebDriverManager.firefoxdriver().setup();
+				tldriver.set(new FirefoxDriver());
+			}
+			else {
+				System.out.println("Please pass a correct browser");
+			}
+			
+			getDriver().manage().deleteAllCookies();
+			getDriver().manage().window().maximize();
+			return getDriver();
+	}
+		
+	//this is used to get the driver with threadlocal
+	public static synchronized WebDriver getDriver() {
+		return tldriver.get();
+	}
 
 }
---
+++ src/test/java/apphooks/ApplicationHooks.java
@@ -0,0 +1,47 @@
+package apphooks;
+
+import java.util.Properties;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import com.qa.factory.DriverFactory;
+import com.qa.util.ConfigReader;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class ApplicationHooks {
+	
+	private DriverFactory driverFactory;
+	private ConfigReader reader;
+	Properties prop;
+	WebDriver driver;
+	
+	@Before(order=0)
+	public void loadConfig() {
+		prop = reader.getProperties();
+	}
+	
+	@Before(order=1)
+	public void initDriver() {
+		String browser = prop.getProperty("browser");
+		driverFactory.initDriver(browser);
+	}
+	
+	@After(order=0)
+	public void quitBrowser() {
+		driver.quit();
+	}
+	
+	@After(order=1)
+	public void tearDown(Scenario scenario) {
+		if(scenario.isFailed()) {
+			String screenshot = scenario.getName().replaceAll("", "_");
+			byte[] sourcepath = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(sourcepath, "image/png", screenshot);
+		}
+	}
+
+}
